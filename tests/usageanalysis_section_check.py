2191db83c387bbaebf4fcc706401039f93edd14c
ilsung-dpsl <ilsung.baek@deepsales.com>
2025-09-29 17:29:49 +0900

    2025.9.29 (월) 탐색하기 UI(LNB 포함) 배포로 인한 usageanalysis_section_check 파일 코드 수정

--- tests/usageanalysis_section_check.py
+++ tests/usageanalysis_section_check.py
@@ -12,8 +12,19 @@ def test_usageanalysis_section_check(page):
     page.wait_for_timeout(1000)
  #   page.get_by_role("button", name="Start Now").click()
  #   page.wait_for_timeout(1000)
-    page.get_by_role("link", name="대시보드").click()
-    page.wait_for_timeout(5000)
+
+    # 20250929 - 탐색하기 ui(LNB 영역) 변경으로 인한 LNB 숨김처리됨 -> LNB 마우스 호버하는 코드 추가 및 수정
+    lnb_hover_target = page.get_by_text("대시보드탐색하기발견하기마이 리스트").first
+    lnb_hover_target.hover()
+    page.wait_for_timeout(2000)
+
+    # 20250929 - LNB > 사이드바 메뉴 펼침  버튼 선택 코드 추가
+    page.get_by_role("button").first.click()
+    page.wait_for_timeout(2000)
+
+    # 20250929 - LNB > 대시보드 메뉴 영역 선택 위치 변경으로 인한 코드 수정
+    page.get_by_role("link", name="대시보드").nth(1).click()
+    page.wait_for_timeout(2000)
 
     assert "산업" in page.content(), "Usage Analysis > 산업 체크 실패"
     assert "부서" in page.content(), "Usage Analysis > 부서 체크 실패"
