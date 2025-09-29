e928ed06592ea71ec551decd8ba92bb56e898492
ilsung-dpsl <ilsung.baek@deepsales.com>
2025-09-30 08:48:14 +0900

    2025.9.30 (화) 탐색하기 UI(LNB 포함) 배포로 인한 dashboard_discover_link_move 파일 코드 수정

--- tests/dashboard_discover_link_move.py
+++ tests/dashboard_discover_link_move.py
@@ -12,7 +12,18 @@ def test_dashboard_discover_link_move(page):
  #   page.wait_for_timeout(1000)
  #   page.get_by_role("button", name="Start Now").click()
     page.wait_for_timeout(1000)
-    page.get_by_role("link", name="대시보드").click()
+
+    # 20250929 - 탐색하기 ui(LNB 영역) 변경으로 인한 LNB 숨김처리됨 -> LNB 마우스 호버하는 코드 추가 및 수정
+    lnb_hover_target = page.get_by_text("대시보드탐색하기발견하기마이 리스트").first
+    lnb_hover_target.hover()
+    page.wait_for_timeout(2000)
+
+    # 20250929 - LNB > 사이드바 메뉴 펼침  버튼 선택 코드 추가
+    page.get_by_role("button").first.click()
+    page.wait_for_timeout(2000)
+
+    # 20250929 - LNB > 대시보드 메뉴 영역 선택 위치 변경으로 인한 코드 수정
+    page.get_by_role("link", name="대시보드").nth(1).click()
     page.wait_for_timeout(2000)
 
     #대시보드 > 발견하기 > 1번 항목 탐색하기 연동 확인
@@ -25,7 +36,8 @@ def test_dashboard_discover_link_move(page):
 
     print("--- 발행일 기준 최신 상위 6개 Discover 노출, 클릭 시 Discover 항목 선택 결과로 연결 1 -> 성공")
 
-    page.get_by_role("link", name="대시보드").click()
+    # 20250930 - LNB 사이드바 메뉴 펼쳐지고 난 후에는 대시보드 메뉴 코드 값이 변경되어 코드 수정
+    page.get_by_role("link", name="대시보드").first.click()
     page.wait_for_timeout(2000)
 
     page.get_by_text("미국 도매 알코올 음료 업계, 직원 수 50-200").click()
