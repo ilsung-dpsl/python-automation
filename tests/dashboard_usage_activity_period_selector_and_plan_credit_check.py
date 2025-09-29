95fbbda7a70784cbfc4d1d5aa8e4a3a9333fd477
ilsung-dpsl <ilsung.baek@deepsales.com>
2025-09-29 17:27:44 +0900

    2025.9.29 (월) 탐색하기 UI(LNB 포함) 배포로 인한 dashboard_usage_activity_period_selector_and_plan_credit_check 파일 코드 수정

--- tests/dashboard_usage_activity_period_selector_and_plan_credit_check.py
+++ tests/dashboard_usage_activity_period_selector_and_plan_credit_check.py
@@ -12,8 +12,20 @@ def test_dashboard_usage_activity_period_selector_and_plan_credit_check(page):
     # page.wait_for_timeout(1000)
     # page.get_by_role("button", name="Start Now").click()
     page.wait_for_timeout(1000)
-    page.get_by_role("link", name="대시보드").click()
-    page.wait_for_timeout(3000)
+
+    # 20250929 - 탐색하기 ui(LNB 영역) 변경으로 인한 LNB 숨김처리됨 -> LNB 마우스 호버하는 코드 추가 및 수정
+    lnb_hover_target = page.get_by_text("대시보드탐색하기발견하기마이 리스트").first
+    lnb_hover_target.hover()
+    page.wait_for_timeout(2000)
+
+    # 20250929 - LNB > 사이드바 메뉴 펼침  버튼 선택 코드 추가
+    page.get_by_role("button").first.click()
+    page.wait_for_timeout(2000)
+
+    # 20250929 - LNB > 대시보드 메뉴 영역 선택 위치 변경으로 인한 코드 수정
+    page.get_by_role("link", name="대시보드").nth(1).click()
+    page.wait_for_timeout(2000)
+
     page.get_by_text("Enterprise").click()
 
 #    print("기간 셀렉터 확인 및 이번주 설정 후 크레딧 사용량 확인 케이스 -> 기능 없음으로 확인 불가")
