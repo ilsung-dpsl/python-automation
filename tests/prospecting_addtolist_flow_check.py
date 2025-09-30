53876013f2f315f52559e940bc6de4dcf35a14f7
ilsung-dpsl <ilsung.baek@deepsales.com>
2025-09-30 11:19:45 +0900

    2025.9.30 (화) 탐색하기 UI(LNB 포함) 배포로 인한 prospecting_addtolist_flow_check 파일 코드 재수정

--- tests/prospecting_addtolist_flow_check.py
+++ tests/prospecting_addtolist_flow_check.py
@@ -19,12 +19,14 @@ def test_prospecting_addtolist_flow_check(page):
     page.get_by_placeholder("예: 일본 화장품 제조사 세일즈 매니저").press("Enter")
     page.wait_for_timeout(6000)
 
-    #page.locator(".w-\\[32px\\] > .w-fit > .p-\\[2px\\]").first.click()
-    page.locator(".w-\\[32px\\].h-\\[48px\\] > .flex").first.click()
+    #20240930 - 탐색하기 ui 변경으로 인한 리드 > 1번, 2번 체크 동작 코드 수정
+    page.locator("div > .w-8 > .flex").first.click()
     page.wait_for_timeout(1000)
-    #page.locator("div:nth-child(2) > div > .w-\\[32px\\] > .w-fit > .p-\\[2px\\]").click()
-    page.locator("div:nth-child(2) > div > .w-\\[32px\\] > .flex").click()
+
+    page.locator("div:nth-child(2) > div > .w-8 > .flex").click()
     page.wait_for_timeout(1000)
+
+
     page.get_by_role("button", name="리스트에 추가").click()
     page.wait_for_timeout(1000)
     page.get_by_role("button", name="새 리스트 생성").click()
@@ -45,12 +47,22 @@ def test_prospecting_addtolist_flow_check(page):
 
     page.get_by_role("button", name="확인", exact=True).click()
     page.wait_for_timeout(5000)
-    page.get_by_role("link").filter(has_text="마이 리스트").click()
-    page.wait_for_timeout(3000)
-    page.get_by_role("link").filter(has_text="마이 리스트").click()
+
+    #20250930 - 탐색하기 ui(LNB 영역) 변경으로 인한 LNB 숨김처리됨 -> LNB 마우스 호버하는 코드 추가 및 수정
+    lnb_hover_target = page.get_by_text("대시보드탐색하기발견하기마이 리스트").first
+    lnb_hover_target.hover()
+    page.wait_for_timeout(2000)
+
+    #20250930 - LNB > 사이드바 메뉴 펼침  버튼 선택 코드 추가
+    page.get_by_role("button").first.click()
+    page.wait_for_timeout(2000)
+
+    #20250930 - LNB > 대시보드 메뉴 영역 선택 위치 변경으로 인한 코드 수정
+    page.get_by_role("link", name="마이 리스트").nth(1).click()
     page.wait_for_timeout(3000)
 
-    page.get_by_text("기본2(2 미확인)백 일성").click()
+    #20250930 - 기본 폴더 선택 동작 코드 수정
+    page.locator("div").filter(has_text=re.compile(r"^기본$")).nth(1).click()
     page.wait_for_timeout(3000)
 
     #Default 폴더 > 데이터 위치 변경으로 인한 리드 데이터 정보 수정 - 20250829
@@ -70,6 +82,7 @@ def test_prospecting_addtolist_flow_check(page):
 
     page.wait_for_timeout(3000)
 
+    #20250930 - 기본 폴더 선택 동작 코드 수정
     page.locator("div").filter(has_text=re.compile(r"^기본$")).nth(1).click()
 
     #Default 폴더 > 리스트 1, 2 체크 동작 코드 수정 - 20250805
