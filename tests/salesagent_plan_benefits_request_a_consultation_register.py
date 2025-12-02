380e7fba0e2c38ff28c7b708cb2db51faeeb1612
ilsung-dpsl <ilsung.baek@deepsales.com>
2025-12-02 17:03:54 +0900

    2025.12.2 (화) salesagent_plan_benefits_request_a_consultation_register.py 세일즈 에이전트 > 플랜 혜택 소개 > 상담 요청하기 문의사항 등록 테스트 스크립트 추가

---
+++ tests/salesagent_plan_benefits_request_a_consultation_register.py
@@ -0,0 +1,67 @@
+import config
+import re
+
+def test_salesagent_plan_benefits_request_a_consultation_register(page):
+    print("---- 90번 - 세일즈 에이전트 > 플랜 혜택 보기 > 상담 요청하기 문의사항 등록 테스트 시작 ----")
+
+    page.goto("https://deepsales.com/ko/intro", wait_until="load", timeout=30000)
+    page.wait_for_timeout(1000)
+
+    page.get_by_role("link", name="세일즈 에이전트 바우처").click()
+    page.wait_for_timeout(1000)
+
+    page.get_by_role("button", name="플랜 확인하기").click()
+    page.wait_for_timeout(500)
+
+    page.locator("#plan").get_by_role("button", name="상담 요청하기").click()
+    page.wait_for_timeout(1000)
+
+    page.get_by_placeholder("이름 입력", exact=True).fill("ILSUNG")
+    page.get_by_placeholder("성 입력").fill("BAEK")
+    page.get_by_placeholder("성 입력").press("Tab")
+    page.wait_for_timeout(500)
+
+    page.get_by_placeholder("회사 이름 입력").fill("DeepSales Test")
+    page.get_by_placeholder("회사 이름 입력").press("Tab")
+    page.wait_for_timeout(500)
+
+    page.get_by_placeholder("직함 입력").fill("QA Manager and Sales Agent")
+    page.get_by_placeholder("직함 입력").press("Tab")
+    page.wait_for_timeout(500)
+
+    page.locator("#react-select-2-input").fill("대한민국")
+    page.locator("#react-select-2-input").press("Enter")
+    page.locator("#react-select-2-input").press("Tab")
+    page.wait_for_timeout(500)
+
+    page.get_by_placeholder("회사 이메일 입력").fill(config.GMAIL_EMAIL)
+    page.get_by_placeholder("회사 이메일 입력").press("Tab")
+    page.locator("#react-select-3-input").fill("82")
+    page.wait_for_timeout(1000)
+    page.get_by_text("+82").click()
+    page.wait_for_timeout(1000)
+
+    page.get_by_placeholder("- 없이 전화번호 입력").fill(config.CARD1_AUTH_PHONE)
+    page.get_by_placeholder("- 없이 전화번호 입력").press("Tab")
+    page.wait_for_timeout(500)
+
+    page.get_by_role("textbox", name="귀하의 사업과 문의사항을 알려주세요").fill("QA 운영 자동화 테스트 입니다. \n세일즈 에이전트 > 플랜 헤택 소개 > 상담 요청하기 > \n문의사항 등록 (크롬)")
+    page.wait_for_timeout(1000)
+
+    page.get_by_role("button", name="제출하기").click()
+    page.wait_for_timeout(1000)
+
+    assert "문의 전송" == page.get_by_text("문의 전송").inner_text(), \
+        "문의 전송 모달 > 타이틀 문구 확인 실패 - 세일즈 에이전트 > 플랜 혜택 소개 > 상담 요청하기 문의 사항 등록 실패 1"
+    assert ("문의하신 내용은 발송되었습니다. 저희 영업팀에서 문의하신 내용을 확인하고 다시 연락드리겠습니다." ==
+            page.get_by_text("문의하신 내용은 발송되었습니다. 저희 영업팀에서 문의하신 내용을 확인하고 다시 연락드리겠습니다").inner_text()), \
+        "문의 전송 모달 > 안내 문구 확인 실패 - 세일즈 에이전트 > 플랜 혜택 소개 > 상담 요청하기 문의 사항 등록 실패 2"
+    assert "확인" == page.get_by_role("button", name="확인", exact=True).inner_text(), \
+        "문의 전송 모달 > [확인] 버튼 확인 실패 - 세일즈 에이전트 > 플랜 혜택 소개 > 상담 요청하기 문의 사항 등록 실패 3"
+
+    page.get_by_role("button", name="확인", exact=True).click()
+    page.wait_for_timeout(1000)
+
+    print("---- 90번 - 세일즈 에이전트 > 플랜 혜택 보기 > 상담 요청하기 문의사항 등록 테스트 시작 -> 성공 ----")
+
+
