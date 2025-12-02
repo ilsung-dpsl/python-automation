b4fa715566c813d40cd8d1c107d71f696374f1d5
ilsung-dpsl <ilsung.baek@deepsales.com>
2025-12-02 16:18:48 +0900

    2025.12.2 (화) salesagent_boucher_modal_ui_check.py salesagent_boucher_modal_link_move_check.py 세일즈 에이전트 바우처 및 정부 지원 헤택 추가로 해당 테스트 스크립트 추가

---
+++ tests/salesagent_boucher_modal_ui_check.py
@@ -0,0 +1,46 @@
+import re
+import config
+import cv2
+import numpy as np
+
+def test_salesagent_boucher_modal_ui_check(page):
+    print("---- 88번 - 세일즈 에이전트 > 플랜 혜택 보기의 바우처 혜택 상세 보기 선택 시 정부가 인증한 AI 기반 해외 영업 지원 서비스 모달 노출 확인 테스트 시작 ----")
+
+    page.goto("https://deepsales.com/ko/intro",wait_until="load", timeout=30000)
+    page.wait_for_timeout(1000)
+
+    page.get_by_role("link", name="세일즈 에이전트 바우처").click()
+    page.wait_for_timeout(1000)
+
+    print("세일즈 에이전트 랜딩 페이지 진입 완료")
+
+    page.get_by_role("button", name="플랜 확인하기").click()
+    page.wait_for_timeout(1000)
+
+    page.get_by_role("button", name="바우처 혜택 상세 보기").click()
+    page.wait_for_timeout(1000)
+
+    print("정부가 인증한 AI 기반 해외 영업 지원 서비스 모달 출력")
+
+    assert "정부가 인증한 AI 기반 해외 영업 지원 서비스" == page.get_by_text("정부가 인증한 AI 기반 해외 영업 지원 서비스").inner_text(), \
+        ("정부가 인증한 AI 기반 해외 영업 지원 서비스 모달 > 타이틀 문구 확인 실패 - "
+         "세일즈 에이전트 > 플랜 혜택 보기의 바우처 혜택 상세 보기 선택 시 정부가 인증한 AI 기반 해외 영업 지원 서비스 모달 노출 확인 실패 1")
+    assert "수출 바우처 사업" == page.get_by_text("수출 바우처 사업").first.inner_text(), \
+        ("정부가 인증한 AI 기반 해외 영업 지원 서비스 모달 > 수출 바우처 사업 1 확인 실패 - "
+         "세일즈 에이전트 > 플랜 혜택 보기의 바우처 혜택 상세 보기 선택 시 정부가 인증한 AI 기반 해외 영업 지원 서비스 모달 노출 확인 실패 2")
+    assert "AI 바우처 사업" == page.get_by_text("AI 바우처 사업").inner_text(), \
+        ("정부가 인증한 AI 기반 해외 영업 지원 서비스 모달 > AI 바우처 사업 3 확인 실패 - "
+         "세일즈 에이전트 > 플랜 혜택 보기의 바우처 혜택 상세 보기 선택 시 정부가 인증한 AI 기반 해외 영업 지원 서비스 모달 노출 확인 실패 3")
+    assert "전통문화 혁신 바우처 사업" == page.get_by_text("전통문화 혁신 바우처 사업").inner_text(), \
+        ("정부가 인증한 AI 기반 해외 영업 지원 서비스 모달 > 전통문화 혁신 바우처 사업 4 확인 실패 - "
+         "세일즈 에이전트 > 플랜 혜택 보기의 바우처 혜택 상세 보기 선택 시 정부가 인증한 AI 기반 해외 영업 지원 서비스 모달 노출 확인 실패 4")
+    assert "바로가기" == page.get_by_role("link", name="바로가기").first.inner_text(), \
+        ("정부가 인증한 AI 기반 해외 영업 지원 서비스 모달 > 바로가기 링크 연동 노출 확인 실패 - "
+         "세일즈 에이전트 > 플랜 혜택 보기의 바우처 혜택 상세 보기 선택 시 정부가 인증한 AI 기반 해외 영업 지원 서비스 모달 노출 확인 실패 5")
+    assert "확인" == page.get_by_role("button", name="확인", exact=True).inner_text(), \
+        ("정부가 인증한 AI 기반 해외 영업 지원 서비스 모달 > [확인] 노출 확인 실패 - "
+         "세일즈 에이전트 > 플랜 혜택 보기의 바우처 혜택 상세 보기 선택 시 정부가 인증한 AI 기반 해외 영업 지원 서비스 모달 노출 확인 실패 6")
+
+    page.wait_for_timeout(1000)
+
+    print("---- 88번 - 세일즈 에이전트 > 플랜 혜택 보기의 바우처 혜택 상세 보기 선택 시 정부가 인증한 AI 기반 해외 영업 지원 서비스 모달 노출 확인 테스트 시작 -> 성공 ----")
