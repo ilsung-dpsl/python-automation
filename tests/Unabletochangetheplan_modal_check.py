2c1cfd0e8af96937e05a4b462f953675170ed272
ilsung-dpsl <ilsung.baek@deepsales.com>
2025-12-11 09:41:07 +0900

    2025.12.11 (목) dashboard_mylist_contacts_count_check.py Unabletochangetheplan_modal_check.py
    계정 정보 변경 및 저장, 확인되지않은 개수 변경

--- tests/Unabletochangetheplan_modal_check.py
+++ tests/Unabletochangetheplan_modal_check.py
@@ -8,8 +8,9 @@ def test_unabletochangetheplan_modal_check(page):
     page.goto("https://deepsales.com/ko/intro",wait_until="load", timeout=50000)
     page.get_by_role("button", name="로그인").click()
     page.wait_for_timeout(1000)
-    page.get_by_placeholder("이메일").fill(config.ENTERPRISE_TEAM_MEMBER_AC)
-    page.get_by_placeholder("비밀번호").fill(config.ENTERPRISE_TEAM_MEMBER_PW)
+    #20251211 - ISS-224 팀관리 이슈로 인해, 팀멤버 계정을 ilsung.baek -> pa20 계정으로 변경
+    page.get_by_placeholder("이메일").fill(config.ENTERPRISE_MEMBER_PA20_ACC)
+    page.get_by_placeholder("비밀번호").fill(config.ENTERPRISE_MEMBER_PA20_PW)
     page.get_by_role("button", name="로그인").click()
     page.wait_for_timeout(1000)
   #  page.get_by_role("button", name="Start Now").click()
