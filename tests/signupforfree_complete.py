6d83d9a7627e784c66b3da30f06b858381073ba8
ilsung-dpsl <ilsung.baek@deepsales.com>
2025-12-12 09:44:33 +0900

    2025.12.12 (금) signupforfree_complete.py 회원가입 완료 -> 회원가입 완료 전까지 프로세스 확인으로 케이스 수정

--- tests/signupforfree_complete.py
+++ tests/signupforfree_complete.py
@@ -42,7 +42,8 @@ def test_signupforfree_complete(page):
     """
     :type page: object
     """
-    print("----- 2번 - 회원가입 완료 스크립트 테스트 시작 -----")
+    ## 20251212 이전 동작 코드에서 사용한 확인용 -> print("----- 2번 - 회원가입 완료 스크립트 테스트 시작 -----")
+    print("----- 20251212 - 회원가입 완료 전까지 프로레스 확인 스크립트 테스트 시작 -----")
 
     page.goto("https://deepsales.com/ko/intro",wait_until="load", timeout=30000)
 #    page.goto("https://dev.deepsales.io/ko/intro",wait_until="load", timeout=30000)
@@ -53,6 +54,9 @@ def test_signupforfree_complete(page):
     page.get_by_role("banner").get_by_role("link", name="세일즈 에이전트").click()
     page.wait_for_timeout(1000)
 
+    # 20251212 - 세일즈 에이전트 진입 완료 확인용 코드 추가
+    print("세일즈 에이전트 페이지 진입 완료")
+
     # 20251203 - 세일즈 에이전트 랜딩 페이지 > 상단 > [플랜 확인하기] 선택 코드 수정
     page.get_by_role("button", name="플랜 확인하기").click()
     page.wait_for_timeout(1000)
@@ -60,6 +64,9 @@ def test_signupforfree_complete(page):
     # 20251203 - 세일즈 에이전트 랜딩 페이지 > 플랜소개 > Scale > [시작하기] 선택 앨리먼트 요소 수정되어 코드 수정
     page.get_by_role("button", name="시작하기").nth(2).click()
     page.wait_for_timeout(1000)
+
+    # 20251212 - 회원가입 페이지 진입 완료 확인용 코드 추가
+    print("회원가입 페이지 진입 완료")
 
     count = read_counter()
     page.get_by_placeholder("예) deepsales@deepsales.com").fill(f"ilsung.baek+pa{count}@deepsales.com")
@@ -87,12 +94,14 @@ def test_signupforfree_complete(page):
     # 5. 원래 페이지로 돌아와 인증번호 입력
     page.bring_to_front()
 
-    #
     page.get_by_placeholder("인증번호를 입력해주세요").click()
     print(f"[Verification number] {code}")
     page.wait_for_timeout(2000)
     page.get_by_placeholder("인증번호를 입력해주세요").fill(code)
     page.get_by_role("button", name="확인").click()
+
+    # 20251212 - 회원가입 > 인증번호 입력 완료 확인용 코드 추가
+    print("회원가입 > 인증번호 입력 완료")
 
     page.get_by_placeholder("성 (영문 입력)").fill("백")
     page.get_by_placeholder("이름 (영문 입력)").fill("일성")
@@ -104,50 +113,65 @@ def test_signupforfree_complete(page):
 
     page.wait_for_timeout(7000)
 
-    page.get_by_role("button", name="가입하기").click()
-    page.wait_for_timeout(5000)
+    #20251212 - 회원가입 완료 전 입력한 값이나, 인증번호 완료 후 인증 안내 문구 등 확인하는 코드 변경 및 추가
+    assert "이메일 인증이 완료되었습니다." == page.get_by_text("이메일 인증이 완료되었습니다").inner_text(), \
+        "인증번호 완료 후 이메일 인증 완료 안내 문구 확인 실패 - 회원가입 완료 전 프로세스 확인 실패 1"
+    assert "백" == page.get_by_placeholder("성 (영문 입력)").input_value(), \
+        "회원가입 > 성 > 입력값 확인 실패 - 회원가입 완료 전 프로세스 확인 실패 2"
+    assert "일성" == page.get_by_placeholder("이름 (영문 입력)").input_value(), \
+        "회원가입 > 이름 > 입력값 확인 실패 - 회원가입 완료 전 프로세스 확인 실패 3"
+    button = page.get_by_role("button", name="가입하기")
+    assert button.is_enabled(), \
+        "회원가입 > 가입하기 버튼 활성화 노출 확인 실패 - 회원가입 완료 전 프로세스 확인 실패 4"
+
+
+#------ 20251212 - 아래의 케이스는 회원가입 > 가입하기 완료 프로세스이나 지속적인 테스트 계정 생성으로 인해 회원가입 테스트 플로우 변경 -----#
+    ### ---- ## 주석 2개가 실제 사용 코드, # 주석 1개는 이전 사용했던 코드나 사용하지 않음 ------
+
+    ##page.get_by_role("button", name="가입하기").click()
+    ##page.wait_for_timeout(5000)
 
    # page.get_by_role("button", name="Start Now").click()
    # page.wait_for_timeout(1000)
 
     # 20251125 - 세일즈 에이전트 페이지 > 상단 > [제품 이용하기] 선택 하는 코드 추가
-    page.get_by_role("button", name="제품 이용하기").click()
-    page.wait_for_timeout(1000)
+    ##page.get_by_role("button", name="제품 이용하기").click()
+    ##page.wait_for_timeout(1000)
 
     #20250930 - 탐색하기 ui(LNB 영역) 변경으로 인한 LNB 숨김처리됨 -> LNB 마우스 호버하는 코드 추가 및 수정
-    lnb_hover_target = page.get_by_text("대시보드탐색하기발견하기마이 리스트").first
-    lnb_hover_target.hover()
-    page.wait_for_timeout(2000)
+    ##lnb_hover_target = page.get_by_text("대시보드탐색하기발견하기마이 리스트").first
+    ##lnb_hover_target.hover()
+    ##page.wait_for_timeout(2000)
 
     #20250930 - LNB > 사이드바 메뉴 펼침  버튼 선택 코드 추가
-    page.get_by_role("button").first.click()
-    page.wait_for_timeout(2000)
+    ##page.get_by_role("button").first.click()
+    ##page.wait_for_timeout(2000)
 
     #20250930 - LNB > 탐색하기 메뉴 영역 선택 위치 변경으로 인한 코드 수정
-    page.get_by_role("link", name="탐색하기").nth(1).click()
-    page.wait_for_timeout(2000)
+    ##page.get_by_role("link", name="탐색하기").nth(1).click()
+    ##page.wait_for_timeout(2000)
 
     #page.get_by_role("paragraph").filter(has_text=re.compile(r"^필터$")).click()
 
-    assert "탐색하기" == page.locator("#desktop-header-slot").get_by_text("탐색하기").inner_text(), \
-        "회원가입 완료 후 탐색하기 이동 > '탐색하기' 타이틀 문구 확인 실패 - 회원가입 완료 실패 1"
-    assert "필터" == page.get_by_text("필터").inner_text(), \
-        "회원가입 완료 후 탐색하기 이동 > 필터 > '필터' 타이틀 문구 확인 실패 - 회원가입 완료 실패 2"
+    ##assert "탐색하기" == page.locator("#desktop-header-slot").get_by_text("탐색하기").inner_text(), \
+    ##    "회원가입 완료 후 탐색하기 이동 > '탐색하기' 타이틀 문구 확인 실패 - 회원가입 완료 실패 1"
+    ##assert "필터" == page.get_by_text("필터").inner_text(), \
+    ##    "회원가입 완료 후 탐색하기 이동 > 필터 > '필터' 타이틀 문구 확인 실패 - 회원가입 완료 실패 2"
 
-    print("회원가입 완료 후 탐색하기 이동 성공")
+    ##print("회원가입 완료 후 탐색하기 이동 성공")
     
-    count += 1
-    write_counter(count)
+    ##count += 1
+    ##write_counter(count)
 
-    print (f"counter : {count}")
+    ##print (f"counter : {count}")
 
     #20250930 - 탐색하기 UI 변경(LNB 레이아웃)으로 인한 대시보드 선택 코드 수정
-    page.get_by_role("link", name="대시보드").nth(1).click()
-    page.wait_for_timeout(6000)
+    ##page.get_by_role("link", name="대시보드").nth(1).click()
+    ##page.wait_for_timeout(6000)
 
-    assert "백 일성님\n환영합니다!" in page.get_by_text("백 일성님 환영합니다!").inner_text(), \
-        "대시보드 환영문구 확인 실패 - 회원가입 실패 3"
-    assert "크레딧 15/" in page.content(), \
-        "대시보드 > 크레딧 보유량 확인 실패 - 회원가입 실패 3"
+    ##assert "백 일성님\n환영합니다!" in page.get_by_text("백 일성님 환영합니다!").inner_text(), \
+    ##    "대시보드 환영문구 확인 실패 - 회원가입 실패 3"
+    ##assert "크레딧 15/" in page.content(), \
+    ##    "대시보드 > 크레딧 보유량 확인 실패 - 회원가입 실패 3"
 
     print("----- 회원가입 완료 스크립트 테스트 시작 -> 성공 -----")
