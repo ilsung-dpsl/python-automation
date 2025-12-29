7a59963f69c939d29ad6bab79b4081394b5f73ba
ilsung-dpsl <ilsung.baek@deepsales.com>
2025-12-29 09:11:27 +0900

    2025.12.29 (월) salesagent_starter_toss_payments_1_2page_input_check.py 테스트 시작 시 번호 86번으로 변경

--- tests/salesagent_starter_toss_payments_1_2page_input_check.py
+++ tests/salesagent_starter_toss_payments_1_2page_input_check.py
@@ -2,7 +2,7 @@ import re
 import config
 
 def test_salesagent_starter_toss_payments_1_2page_input_check(page):
-    print("---- 84번 - 세일즈 에이전트 > Starter > 구독 결제 완료 전까지 동작 확인 ----")
+    print("---- 86번 - 세일즈 에이전트 > Starter > 구독 결제 완료 전까지 동작 확인 ----")
 
     # 20251209 - url 이동 시 로드 타임아웃 50초 코드로 수정
     page.goto("https://deepsales.com/ko/intro", wait_until="load", timeout=50000)
