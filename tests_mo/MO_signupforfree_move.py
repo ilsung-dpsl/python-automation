e337d751f9f55747d3ab1357ea643b21f37c857a
ilsung-dpsl <ilsung.baek@deepsales.com>
2026-01-12 15:01:38 +0900

    2026.1.12 (월) MO_signupforfree_move.py 코드 수정

--- tests_mo/MO_signupforfree_move.py
+++ tests_mo/MO_signupforfree_move.py
@@ -8,6 +8,7 @@ def test_MO_signupforfree_move(mobile_page):
     mobile_page.wait_for_timeout(3000)
 
     mobile_page.wait_for_selector("text=DeepSales에 오신 것을 환영합니다!", timeout=3000)
+    mobile_page.wait_for_timeout(1000)
 
     assert "딥세일즈 | 가입하기" in mobile_page.title(), "회원가입 페이지 타이틀 확인 실패 - 회원가입 페이지 이동 실패 1"
     print("--- 1번 - MO 회원가입 페이징 이동 완료 -> 성공")
