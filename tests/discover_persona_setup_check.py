1a9681431b68f110e8398db1b3d7766f6baf3158
ilsung-dpsl <ilsung.baek@deepsales.com>
2026-02-10 08:55:10 +0900

    2026.2.10 (화) discover_persona_setup_check.py 방어코드 수정 및 추가

--- tests/discover_persona_setup_check.py
+++ tests/discover_persona_setup_check.py
@@ -19,11 +19,11 @@ def test_discover_persona_setup_check(page):
     page.wait_for_timeout(2000)
 
     # 20250930 - LNB > 사이드바 메뉴 펼침  버튼 선택 코드 추가
-    page.get_by_role("button").first.click()
+    page.get_by_role("button").first.click(timeout=10000)
     page.wait_for_timeout(2000)
 
     # 20250930 - LNB > 발견하기 메뉴 영역 선택 위치 변경으로 인한 코드 수정
-    page.get_by_role("link", name="발견하기").nth(1).click()
+    page.get_by_role("link", name="발견하기").nth(1).click(timeout=10000)
     #대기 시작 3초로 수정 - 20250829
     page.wait_for_timeout(3000)
 
@@ -73,7 +73,8 @@ def test_discover_persona_setup_check(page):
 
     print("새 페르소나 생성 후 페르소나 관리 모달 > 생성한 페르소나 목록 확인 전")
 
-    page.wait_for_timeout(2000)
+    #20260210 - 대기시간 2초 -> 3초로 증가
+    page.wait_for_timeout(3000)
 
     assert "새 페르소나를 만들어 볼까요?" == page.get_by_text("새 페르소나를 만들어 볼까요?").inner_text(), \
         "새 페르소나 생성한 제목 노출 실패 - 퍼소나 설정 안내 모달 및 퍼소나 설정 추가 실패 4"
@@ -82,13 +83,13 @@ def test_discover_persona_setup_check(page):
 
     #20250105 - 해당 앨리먼트 노출 할떄까지 타임아웃 대기 10초 후 나타나면 클릭으로 코드 수정
     page.get_by_role("article").filter(has_text="새 페르소나를 만들어 볼까요?Home Health").get_by_role("button").click(timeout=10000)
-    page.wait_for_timeout(500)
+    page.wait_for_timeout(1000)
     #20250105 - 해당 앨리먼트 노출 할떄까지 타임아웃 대기 10초 후 나타나면 클릭으로 코드 수정
     page.get_by_role("button", name="삭제").click(timeout=10000)
-    page.wait_for_timeout(500)
+    page.wait_for_timeout(1000)
     #20250105 - 해당 앨리먼트 노출 할떄까지 타임아웃 대기 10초 후 나타나면 클릭으로 코드 수정
     page.locator("header").filter(has_text="페르소나 관리").get_by_role("button").click(timeout=10000)
-    page.wait_for_timeout(500)
+    page.wait_for_timeout(1000)
 
     print("새 페르소나 생성 후 페르소나 관리 모달 > 생성한 페르소나 목록 정상 추가 확인 완료")
 
